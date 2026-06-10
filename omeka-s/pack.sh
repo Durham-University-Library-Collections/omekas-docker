be77e0dd2fb476d0a10a5c69f3eaa558fd37f675
Matthew Phillips <m.e.phillips@durham.ac.uk>
2026-06-10 11:15:23 +0100

    Add script for packing everything up to transfer to server.

---
+++ omeka-s/pack.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Creates a tar file to transfer to our server for unpacking there.
+
+TARFILE=/var/www/omeka.tar
+
+# Add the Omeka application and any project files
+cd /var/www
+tar --exclude=html/volume --exclude=html/helper --exclude=html/logs --exclude=html/files -cvf $TARFILE html projects
+
+# Add the installation code which we need to run on the server
+cd /
+tar -rvf $TARFILE opt install-omekas.sh
+
+# Place the packaged content somewhere we can access it from outside Docker.
+mv $TARFILE /var/www/projects
