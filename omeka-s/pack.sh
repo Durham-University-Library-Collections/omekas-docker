0e50f5f763574437b2bcf82fb2724801405174c2
Matthew Phillips <m.e.phillips@durham.ac.uk>
2026-06-10 16:39:19 +0100

    Handle config files better

--- omeka-s/pack.sh
+++ omeka-s/pack.sh
@@ -6,7 +6,8 @@ TARFILE=/var/www/omeka.tar
 
 # Add the Omeka application and any project files
 cd /var/www
-tar --exclude=html/volume --exclude=html/helper --exclude=html/logs --exclude=html/files -cvf $TARFILE html projects
+tar --exclude=html/volume --exclude=html/helper --exclude=html/logs --exclude=html/files --exclude html/config -cvf $TARFILE html projects
+tar -rvf $TARFILE html/config/local.config.php
 
 # Add the installation code which we need to run on the server
 cd /
